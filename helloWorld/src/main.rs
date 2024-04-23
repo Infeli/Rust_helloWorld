adbfe4d430fb2a53abfbba68e984b4180fd40a3f
Daniel Pech <Pech.Daniel@seznam.cz>
2024-04-23 21:44:52 +0200

    2nd commit. Adjusted

--- helloWorld/src/main.rs
+++ helloWorld/src/main.rs
@@ -1,4 +1,17 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
-    println!("How you doing?!");
+    println!("-------------");
+    println!("Jak se jmenuješ?");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+
+    print!("Ahoj {name}, jak se daří?!");
 }
